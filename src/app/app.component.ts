07700e0d6d5c24d474ba389ff55ad59ffbb16f16
Mahendra Sahoo <mahendrasahoo@Mahendras-MBP.domain.name>
2020-11-24 19:49:53 +0530

    2nd angular lecture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'skinetClient';
+export class AppComponent implements OnInit {
+  title = 'skinet';
+
+  constructor(){
+    
+  }
+  
+
+  ngOnInit(): void {
+    
+  }
 }
